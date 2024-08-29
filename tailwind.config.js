b2fd81f7ebaa15d6268e84d6ca801a43d11758c8
“paragk” <paragk.spaceo@gmail.com>
2024-08-29 12:58:15 +0530

    Upload init setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const flowbite = require("flowbite-react/tailwind");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", flowbite.content()],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbite.plugin()],
+};
